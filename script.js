2ec273dc866bfc80ffaab4a94911524ff8dead73
Mathew Vieira <mvieirabezerra@gmail.com>
2025-05-13 00:47:54 -0300

    feat/mathew

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const toggleBtn = document.getElementById("menu-toggle");
-const sidebar = document.getElementById("sidebar");
-const themeToggle = document.getElementById("theme-toggle");
-const themeIcon = themeToggle.querySelector("i");
+const toggleBtn = document.getElementById('menu-toggle')
+const sidebar = document.getElementById('sidebar')
+const themeToggle = document.getElementById('theme-toggle')
+const themeIcon = themeToggle.querySelector('i')
 
-toggleBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
-});
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('active')
+})
 
-themeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  if (document.body.classList.contains("dark")) {
-    themeIcon.classList.replace("fa-moon", "fa-sun");
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark')
+  if (document.body.classList.contains('dark')) {
+    themeIcon.classList.replace('fa-moon', 'fa-sun')
   } else {
-    themeIcon.classList.replace("fa-sun", "fa-moon");
+    themeIcon.classList.replace('fa-sun', 'fa-moon')
   }
-});
+})
